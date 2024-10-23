9bf747965a19e9a3a13f194d680cdc57246da3d5
Daniel Simão <156852643+danssimao@users.noreply.github.com>
2024-10-23 17:51:54 -0300

    Update invariant.ts

--- src/invariant.ts
+++ src/invariant.ts
@@ -2,8 +2,10 @@
   A easy way to throw error without need lots of nested if/else statements
 */
 
-export const invariant(condition: boolean, message?: string): never {
+const invariant = (condition: boolean, message?: string): never => {
   if (!condition) {
     throw Error(message);
   }
 }
+
+export default invariant;
